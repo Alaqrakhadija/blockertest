61015abc07990179d7aba93e7085d83acf807aac
Alaqrakhadija <alaqrakhadija@gmail.com>
2023-04-10 11:11:25 +0200

    feat: add new feature

--- blockertest/main.py
+++ blockertest/main.py
@@ -4,12 +4,16 @@ import typer
 app = typer.Typer()
 
 
-
-
 @app.command()
 def shoot():
     """
     Shoot the portal gun
     """
     typer.echo("Shooting portal gun")
+
+@app.command()
+def greet():
+    typer.echo("greeting")
+
+
 
